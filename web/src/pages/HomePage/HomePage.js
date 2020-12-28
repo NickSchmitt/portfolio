f845f7ab872cb0fdfd8344bb4978eb2d1017efe0
nickschmitt <nickschmitt@gmail.com>
2020-12-27 22:54:09 -0800

    Init cCommit

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import BlogLayout from 'src/layouts/BlogLayout'
+import BlogPostCell from 'src/components/BlogPostsCell'
+const HomePage = () => {
+  return (
+    <BlogLayout>
+      <BlogPostCell />
+    </BlogLayout>
+  )
+}
+
+export default HomePage
